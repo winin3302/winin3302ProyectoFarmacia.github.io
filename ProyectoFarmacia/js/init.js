c8e7cca117f15ce56debe21fd310321846a922fe
Martinez Flores Jose Eduardo 5NV50 <153573488+winin3302@users.noreply.github.com>
2024-01-08 11:55:17 -0600

    Update init.js

--- ProyectoFarmacia/js/init.js
+++ ProyectoFarmacia/js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+     apiKey: "AIzaSyCIAr46iG1ISNOqNRFWlZ3YoLwR0FKxgn4",
+  authDomain: "proyfarmacia-c7c6c.firebaseapp.com",
+  projectId: "proyfarmacia-c7c6c",
+  storageBucket: "proyfarmacia-c7c6c.appspot.com",
+  messagingSenderId: "805989993824",
+  appId: "1:805989993824:web:e534ac0665ff3a4a4a2205",
+  measurementId: "G-7Y1BEY8YND"
 });
